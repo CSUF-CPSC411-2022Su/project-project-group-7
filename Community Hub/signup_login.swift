32a5646def948631bd7c60a2d272b8a5b02da5f4
x562372389 <guan260@hotmail.com>
2022-06-08 11:36:42 -0700

    Created signup and login forms

---
+++ Community Hub/signup_login.swift
@@ -0,0 +1,16 @@
+//
+//  signup_login.swift
+//  Community Hub
+//
+//  Created by Alan Guan on 6/8/22.
+//
+
+import Foundation
+
+struct SignUp {
+  // Add code for sign up form here
+}
+
+struct LogIn {
+  // Add code for log in form here
+}
