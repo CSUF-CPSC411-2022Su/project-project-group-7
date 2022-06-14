39eaed365e1a04e0bdbc6b6522a9a5d68ba7dc37
x562372389 <guan260@hotmail.com>
2022-06-13 17:11:52 -0700

    Created a basic login page and added classes to represent data used in sign up/login page

--- Community Hub/ContentView.swift
+++ Community Hub/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Text("Hello, world!")
+//            .padding()
+      LogInPage()
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct LogInPage: View {
+  @State var userName: String = ""
+  @State var password: String = ""
+  
+  var body: some View {
+    VStack {
+      VStack {
+        Text("User Login").padding()
+        
+        HStack {
+          Spacer()
+          Text("Username: ")
+          TextField("Enter your user name", text: $userName)
+          Spacer()
+        }
+        
+        HStack {
+          Spacer()
+          Text("Password: ")
+          TextField("Enter your password", text: $password)
+          Spacer()
+        }
+        
+        
+        HStack {
+          Text("Don't have an account?")
+          Button (action: {
+            print("Test message for sign-up button")
+          }) {
+            Text("Sign Up")   // This button should redirect the user to the SignUp page
+          }
+        }.padding() // Use padding() here to make some space between this HStack and the above HStack that has the password TextField. Might change this later if there's a better spacing option.
+        
+        Spacer() // This spacer function makes all the content in this VStack go to the top of the page
+      }
+    
+      
+    }
+  }
 }
--- Community Hub/SignUpLogIn.swift
+++ Community Hub/SignUpLogIn.swift
@@ -7,10 +7,39 @@
 
 import Foundation
 
-struct SignUp {
-  // Add code for sign up form here
+enum UserType {
+  case volunteer
+  case commercial
 }
+
+class SignUpInformation {
+  // Add code for sign up form here
+  var username: String
+  var password: String
+  var firstName: String
+  var lastName: String
+  var email: String
+  var address: String
+  var interests: String
+  var userType: UserType?
+  
+  init () {
+    self.username = ""
+    self.password = ""
+    self.firstName = ""
+    self.lastName = ""
+    self.email = ""
+    self.address = ""
+    self.interests = ""
+    self.userType = nil
+  }
+  
+}
+
+
 
 struct LogIn {
   // Add code for log in form here
+  var userName: String
+  var password: String
 }
