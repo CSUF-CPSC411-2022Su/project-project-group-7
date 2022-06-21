7bc54612456ef8594a248ffd454ff0998ffd54f2
x562372389 <guan260@hotmail.com>
2022-06-20 19:37:44 -0700

    Cleaned up a bit

--- Community Hub/ContentView.swift
+++ Community Hub/ContentView.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Text("Hello, world!")
-//            .padding()
       LogInPage()
     }
 
@@ -22,43 +20,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct LogInPage: View {
-  @State var userName: String = ""
-  @State var password: String = ""
-  
-  var body: some View {
-    VStack {
-      VStack {
-        Text("User Login").padding()
-        
-        HStack {
-          Spacer()
-          Text("Username: ")
-          TextField("Enter your user name", text: $userName)
-          Spacer()
-        }
-        
-        HStack {
-          Spacer()
-          Text("Password: ")
-          TextField("Enter your password", text: $password)
-          Spacer()
-        }
-        
-        
-        HStack {
-          Text("Don't have an account?")
-          Button (action: {
-            print("Test message for sign-up button")
-          }) {
-            Text("Sign Up")   // This button should redirect the user to the SignUp page
-          }
-        }.padding() // Use padding() here to make some space between this HStack and the above HStack that has the password TextField. Might change this later if there's a better spacing option.
-        
-        Spacer() // This spacer function makes all the content in this VStack go to the top of the page
-      }
-    
-      
-    }
-  }
-}
