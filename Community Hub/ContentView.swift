95bf1829a7c1671b7bd0121062559338d6c90ff8
x562372389 <guan260@hotmail.com>
2022-06-27 10:57:44 -0700

    Edited ContentView to avoid conflict

--- Community Hub/ContentView.swift
+++ Community Hub/ContentView.swift
@@ -4,14 +4,24 @@
 //
 //  Created by csuftitan on 6/8/22.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var manager = CommunityPost()
     var body: some View {
-      LogInPage()
+        TabView {
+            CommunityPostList()
+                .tabItem {
+                    Image(systemName: "info")
+                    Text("View Post")
+                }
+            CommunityPostView()
+                .tabItem {
+                    Image(systemName: "plus")
+                    Text("Create Post")
+                }
+        }.environmentObject(manager)
     }
-
 }
 
 struct ContentView_Previews: PreviewProvider {
